8140ab6c449a5c51088a1d560155589e1d75091e
Aadarsh <86600758+cleff27@users.noreply.github.com>
2022-01-27 19:28:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
